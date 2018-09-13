9729705cde8b4f8792dff715d5561f40ee983a22
Bhargav Bolla <hire.bhargav@gmail.com>
2018-09-12 21:00:30 -0700

    demo StreamTokenizer, FileWriter

--- src/main/java/demo/java/io/demoIO.java
+++ src/main/java/demo/java/io/demoIO.java
@@ -12,22 +12,39 @@ public class demoIO {
 
         int t;
         int words = 0;
-        while((t = streamTokenizer.nextToken()) != StreamTokenizer.TT_EOF) {
-            switch(t) {
+        while ((t = streamTokenizer.nextToken()) != StreamTokenizer.TT_EOF) {
+            switch (t) {
                 case StreamTokenizer.TT_WORD:
                     System.out.println(streamTokenizer.sval);
                     words++;
                     break;
             }
-
-
         }
 
-        System.out.println("Num Words: "+  words);
+        System.out.println("Num Words: " + words);
+    }
+
+
+    private static void demoFileWriter() throws IOException {
+        FileWriter fileWriter = new FileWriter("demo_files/fileWriter");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+        bufferedWriter.write("some text");
+        bufferedWriter.flush();
+
+        fileWriter.close();
+
+        FileReader fileReader = new FileReader("demo_files/fileWriter");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String t;
+        while ((t = bufferedReader.readLine()) != null) {
+            System.out.println(t);
+        }
     }
 
 
     public static void main(String[] args) throws IOException {
         demoStreamTokenizer();
+        demoFileWriter();
     }
 }
